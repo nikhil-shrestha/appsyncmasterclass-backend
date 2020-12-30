952d5650f602281f3f2d35ee0152ea7498e90a17
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2020-12-30 13:22:53 +0545

    acceptance test for tweet mutation

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -204,6 +204,34 @@ const a_user_calls_getImageUploadUrl = async (user, extension, contentType) => {
   return url;
 };
 
+const a_user_calls_tweets = async (user, text) => {
+  const tweet = `mutation tweet($text: String!) {
+    tweet(text: $text) {
+      id
+      createdAt
+      text
+      replies
+      likes
+      retweets
+    }
+  }`;
+  const variables = {
+    text
+  };
+
+  const data = await GraphQL(
+    process.env.API_URL,
+    tweet,
+    variables,
+    user.accessToken
+  );
+  const newTweet = data.tweet;
+
+  console.log(`[${user.username}] - posted new tweet`);
+
+  return newTweet;
+};
+
 module.exports = {
   we_invoke_confirmUserSignup,
   we_invoke_getImageUploadUrl,
@@ -212,5 +240,6 @@ module.exports = {
   a_user_calls_getMyProfile,
   a_user_calls_editMyProfile,
   a_user_calls_getImageUploadUrl,
-  we_invoke_tweet
+  we_invoke_tweet,
+  a_user_calls_tweets
 };
